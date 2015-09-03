8cd9b5edbc7baa7177cee3b7efba89ed7258b8f9
White, Donald <Donald.White@disney.com>
2015-09-03 13:44:10 -0700

    Updated to use the dev versions of angular, http, and router

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,9 @@ var PATHS = {
       'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
       'node_modules/systemjs/dist/system-csp-production.src.js',
       'node_modules/reflect-metadata/Reflect.js',
-      'node_modules/angular2/bundles/angular2.js'
+      'node_modules/angular2/bundles/angular2.dev.js',
+      'node_modules/angular2/bundles/http.dev.js',
+      'node_modules/angular2/bundles/router.dev.js'
     ]
 };
 
